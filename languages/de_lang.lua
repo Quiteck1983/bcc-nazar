1b414c3dda9a4204c03eb83bbd369802387ec54c
Quiteck1983 <53975057+Quiteck1983@users.noreply.github.com>
2024-08-23 09:02:50 +0200

    Create de_lang.lua

---
+++ languages/de_lang.lua
@@ -0,0 +1,34 @@
+---------------------------------- German -------------------------------------------
+Locales["de_lang"] = {
+
+    TreasureBlipName = 'Schatz?',
+    TreasurePromptTitle = 'Durchsuche die Truhe nach Hinweisen',
+    TreasurePrompt_search = "öffnen",
+    TalkToNPCText = 'Drücke "G" um zu sehen, was Frau Nazar anbietet',
+    Menu_SubTitle_Hint = 'Kaufhinweise',
+    Menu_SubTitle_Sell = 'Dinge verkaufen',
+    Menu_SubTitle_Buy = 'Gegenstände kaufen',
+    Nazar = 'Madam Nazar',
+    input_button = "bestätigen",
+    input_placeholder = "Betrag eingeben",
+    input_header = "Menge",
+    HintNotify = "Ein Hinweis auf einen Schatz wurde markiert",
+    NoHintNotify = "Ich habe derzeit nichts anzubieten, komme später wieder",
+    NoItem = "Du hast diesen Artikel nicht.",
+    NoMoney = "Du hast nicht genug Geld.",
+    NoGold = "Du hast nicht genug Gold", -- added by mrtb
+    NoCurrency = "Du hast  nicht genug der benötigten Währung",
+    ChestLooted = 'Erhaltene Artikel: ',
+    Pickup = "Sammeln",
+    YouCollected = "DU hast eine Karte gesammelt",
+    CardCollected = "Karte gesammelt",
+    CantCollectCard = "Bitte warte, jemand hat diese Karte kürzlich abgeholt",
+    StackFull = "Du kannst diesen Artikel nicht mehr tragen",
+    InvFull = "Du keine Gegenstände mehr transportieren",
+
+    CraftCard = "Sammelkarten packen",
+    NotEnoughCards = "Du hast nicht genug Karten : ",
+    PackedCards = "Kartenset gesammelt : ",
+    UnpackedCards = "Kartenset ausgepackt : ",
+    NoCardBox = "Du hast keine Sammelkartenbox."
+}
